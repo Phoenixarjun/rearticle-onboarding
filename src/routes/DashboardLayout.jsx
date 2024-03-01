224524a4d678786e8d01126a4b51ca0286378c68
Shreeraam G <shreeram0312@gmail.com>
2024-03-01 11:18:57 +0530

    Added Images in All Projects Page

--- src/routes/DashboardLayout.jsx
+++ src/routes/DashboardLayout.jsx
@@ -3,8 +3,8 @@ import { Link, Outlet } from "react-router-dom";
 
 const DashboardLayout = () => {
   return (
-    <div className="flex">
-      <div className="flex flex-col gap-y-4">
+    <div className="flex h-svh">
+      <div className="flex flex-col gap-y-4 w-1/5">
         <Link to="/dashboard">All Projects</Link>
         <Link to="/dashboard/my-projects">My Projects</Link>
         <Link to="/dashboard/shared-projects">Shared Projects</Link>
--- src/routes/project-pages/AllProjectsPage.jsx
+++ src/routes/project-pages/AllProjectsPage.jsx
@@ -1,5 +1,13 @@
+import Incognito from "/Incognito.png";
+import Joker from "/Joker PFP.jpg";
+
 const AllProjectsPage = () => {
-  return <div>AllProjectsPage</div>;
+  return (
+    <div className="flex gap-x-8 items-center h-full justify-center w-full">
+      <img src={Incognito} alt="" className="h-60" />
+      <img src={Joker} alt="" className="h-60" />
+    </div>
+  );
 };
 
 export default AllProjectsPage;
